06791d84834605c0ab664334968f937db0796d8f
Marcelo Alejandro Asevedo <116969032+MarceloAsevedo@users.noreply.github.com>
2023-07-17 21:07:19 -0300

    Ejercicio 1 segunda parte

---
+++ Ejercicios Open bootcamp/src/Paquete2daParte/MainEJ1.java
@@ -0,0 +1,30 @@
+
+package Paquete2daParte;
+
+
+public class MainEJ1 {
+    public static void main(String[] args) {
+    int dato1 = 1;
+    double dato2 = 2.4;
+    char dato3 = 'a';
+    byte dato4 = 7;
+    float dato5 = (float)2.954;
+    boolean dato6= dato1<dato2;
+    long dato7 = 35+11;
+    String dato8 = "Este es el dato 8";
+    
+    
+    System.out.println(dato1);
+    System.out.println(dato2);
+    System.out.println(dato3);
+    System.out.println(dato4);
+    System.out.println(dato5);
+    System.out.println(dato6);
+    System.out.println(dato7);
+    System.out.println(dato8);
+    
+    
+        
+    }
+    
+}
